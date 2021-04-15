ec573817e90072e03734756e4f07cf7ce5c41551
vamsi <marreddyvamsi100@gmail.com>
2021-04-15 23:04:05 +0530

    change in ejs

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
-
-const ejs = require("ejs");
-const expressLayouts = require("express-ejs-layouts");
 const app = express();
+
+const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
 
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 
@@ -17,7 +16,10 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
